bf4feebd290bed962c7ba4d3497bba0082174918
Ritanzwe <bidorita@gmail.com>
2024-04-10 22:24:08 +0200

    what

--- api/message/index.js
+++ api/message/index.js
@@ -2,10 +2,20 @@ const cars = require('./cars.json');
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-  
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        add:"massambe",
-        body: cars
-    };
+    if (req.method==="GET"){
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            add:"massambe",
+            body: cars
+        };
+    }
+    else{
+        cars.push(req.body);
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            add:"massambe",
+            body: cars
+        };
+    }
+    
 }
